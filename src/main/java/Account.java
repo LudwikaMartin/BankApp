ff52fbbf0202c1c5dc56719e13425280fbc60c59
Ludwisia1 <Ludwisia123456@example.com>
2022-04-26 18:30:28 +0200

    add get and set for Surname and AccNumber

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -7,13 +7,24 @@ public class Acount {
 
     public String getName(){
         return name;
-
     }
     public void setName(String name){
         this.name = name;
-
-
     }
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public String getAccNumber(){
+        return accNumber;
+    }
+    public void setAccNumber(String accNumber){
+        this.accNumber = accNumber;
+    }
+
+
 
 
 }
