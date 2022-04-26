49053684040b3a8379be12c54443f377e69953ff
Ludwisia1 <Ludwisia123456@example.com>
2022-04-26 18:52:06 +0200

    create account method

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -1,5 +1,13 @@
+import javax.security.auth.login.AccountException;
+import java.util.Scanner;
+
 public class BankApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Account account = new Account();
+
+        account.createAccount();
+
 
     }
 }
