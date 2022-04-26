b6f9105680b123df00bdbb8ab32c8154a018a044
Ludwisia1 <Ludwisia123456@example.com>
2022-04-26 20:40:52 +0200

    Exit app

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -4,28 +4,45 @@ import java.util.Scanner;
 public class BankApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello in our bank!");
+
         Account account = new Account();
         account.createAccount();
 
-        System.out.println("Hello in our bank!");
-        System.out.println("1. Display account details");
-        System.out.println("2. Deposit money");
-        System.out.println("3. Withdrawal money");
-        System.out.println("Enter your choice");
 
         int choice;
-        choice = scanner.nextInt();
-        switch (choice){
-            case 1:
-                account.showAccount();
+        do {
 
-            case 2:
-                account.depositMoney();
+            System.out.println("1. Display account details");
+            System.out.println("2. Deposit money");
+            System.out.println("3. Withdrawal money");
+            System.out.println("4. Exit appBank");
+            System.out.println("Enter your choice");
 
-            case 3:
-                account.withdrawal();
 
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    account.showAccount();
+                    break;
+
+                case 2:
+                    account.depositMoney();
+                    break;
+
+                case 3:
+                    account.withdrawal();
+                    break;
+
+                case 4:
+                    System.out.println("Bye, bye");
+                    break;
+
+            }
         }
+            while (choice != 4);
+
 
     }
 
